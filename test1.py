778d9300d05ce0a9e7d3b6376157e76870561b5c
Tao_glb <2100012931_glb@stu.pku.edu.cn>
2022-01-17 20:27:22 +0800

    The third

--- test1.py
+++ test1.py
@@ -2,7 +2,9 @@ import sys
 def default():
     print("Hello")
 def main():
-    if sys.argv[1]=="cat":
+    if sys.argv[1]=="dog":
+        print("dog")
+    elif sys.argv[1]=="cat":
         print("cat")
     else:
         default()
