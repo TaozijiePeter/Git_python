a94370ed6229218579eace78c57d970e73446637
Tao_glb <2100012931_glb@stu.pku.edu.cn>
2022-01-17 20:33:34 +0800

    The second second

--- test1.py
+++ test1.py
@@ -2,7 +2,9 @@ import sys
 def default():
     print("Hello")
 def main():
-    if sys.argv[1]=="cat":
+    if sys.argv[1]=="wolf":
+        print("wolf")
+    elif sys.argv[1]=="cat":
         print("cat")
     else:
         default()
